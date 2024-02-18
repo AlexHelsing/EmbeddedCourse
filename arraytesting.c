afb52956965bc0ab400cc77d844a6c1eac870842
Alexander <helsinga@student.chalmers.se>
2024-02-18 13:54:06 +0100

    E4 upload and array testing with pointers etc

---
+++ arraytesting.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define MAX 100
+
+int array[MAX]; 
+int arrayLength = sizeof(array) - sizeof(array[0]);
+int* ptrArrayLength = &arrayLength;
+
+void generateRandomArray(int array[], int *size){
+    int i;
+    int num;
+    for(i = 0; i < *size; i++){
+        num = rand() % 100 + 1;
+        array[i] = num;
+    }
+}
+
+void printArray(int array[], int *size){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    srand(time(NULL));
+
+    generateRandomArray(array, ptrArrayLength);
+
+    printf("%p", array);
+    printf("\n");
+    printf("%d", array[0]);
+    printf("\n");
+    printf("%d", array[MAX - 1]);
+    printf("\n");
+    printf("%lu", sizeof(int));
+    printf("\n");
+    printf("%lu", (sizeof(array) / sizeof(array[0])) * sizeof(int));
+
+
+    return 0;
+}
